b9767671b295f5890ed5fdbf1776ac12db031b43
pearlypaul2712 <pearlymathew27212@gmail.com>
2025-08-17 08:25:19 -0500

    Sprint13 commit

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
-// routes/users.js
-const router = require("express").Router();
-const { getCurrentUser, updateUser } = require("../controllers/users");
+  // routes/users.js
+  const router = require("express").Router();
+  const { getCurrentUser, updateUser } = require("../controllers/users");
 
-router.get("/me", getCurrentUser);
-router.patch("/me", updateUser);
+  router.get("/me", getCurrentUser);
+  router.patch("/me", updateUser);
 
-module.exports = router;
+  module.exports = router;
